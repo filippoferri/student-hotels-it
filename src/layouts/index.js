5f72ab0c1587b4d6ef8ccc9e960f39b3b965a2fc
Filippo Ferri <hello@filippoferri.co>
2018-06-21 14:19:59 +0200

    changed logo

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Student Hotels Italia: hotels a misura di studente" />
     <Navbar />
     <div>{children()}</div>
   </div>
